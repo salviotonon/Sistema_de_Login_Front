ea92a33b9e2fb96b9914acf0d8d5ffb2ab8c110b
salviotonon <salvio.tonon@gmail.com>
2022-10-31 17:49:00 -0300

    add button login page

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 
 export const InputGroup = styled.div``;
 
-export const InputLabel = styled.span``;
+export const InputLabel = styled.span`
+    font-size: ;
+`;
 
-export const InputBoxContainer = styled.div``;
-
-export const InputStyled = styled.input``;
-
+export const InputBoxContainer = styled.div`
+    background-color: transparent;
+`;
+export const InputStyled = styled.input`
+    outline: none;
+    border: none;
+    background-color: ${({theme}) => theme.colors.slate[600]};
+`;
 export const InputErrorFeedback = styled.span``;
+
+
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,32 @@
-export const Login = () => (
-  <h1>Login</h1>
-);
+import { Heading } from "../../components/Heading";
+import { Input } from "../../components/Input";
+import { LabelButton } from "../../components/LabelButton";
+import { Text } from "../../components/Text"
+import { Button } from "../../components/Button"
+
+import * as S from "./styles"
+
+import { User, LockKey} from 'phosphor-react';
+
+export const Login = () => {
+
+  return (
+    <S.Container>
+      <div className="content">
+        <Heading>Login</Heading>
+        <Text className="subtitle">Faça login para continuar</Text>
+        <Input 
+        labelName="Nome do usuário"
+        icon={User}
+        ></Input>
+        <Input
+          labelName="Senha"
+          icon={LockKey}
+        ></Input>
+        <LabelButton to="/Signup">Não possui conta?</LabelButton>
+        <LabelButton to="/Signup">Esqueceu sua senha?</LabelButton>
+        <Button>Login</Button>
+      </div>
+    </S.Container>
+  )
+};
