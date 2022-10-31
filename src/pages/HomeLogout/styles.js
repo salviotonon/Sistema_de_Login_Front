d2a3fe5be9cb433f2a52580254df4a51ca34b3f5
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-10-31 00:55:09 -0300

    homelogout finished

--- src/pages/HomeLogout/styles.js
+++ src/pages/HomeLogout/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 2.4rem 0;
 
   h1 {
     margin-bottom: 0.8rem;
@@ -19,5 +20,9 @@ export const Container = styled.div`
     gap: 1.2rem;
 
     margin-top: 3.6rem;
+
+    button {
+      width: 40rem;
+    }
   }
 `;
