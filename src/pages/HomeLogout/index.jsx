0801f70fb6d5934b6f3bdfffafad8d26c4ccef35
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-10-31 01:22:26 -0300

    removed unused import

--- src/pages/HomeLogout/index.jsx
+++ src/pages/HomeLogout/index.jsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import { Button } from '../../components/Button';
 import { Heading } from '../../components/Heading';
 import { Text } from '../../components/Text';
