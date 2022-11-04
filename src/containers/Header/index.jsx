2724467da510d17fc05c5b352ce0284f5090be65
Felipstein <felipstein.oliveira@gmail.com>
2022-11-04 17:42:13 -0300

    header authentication version added

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,40 +1,37 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 import felipeAvatar from '../../assets/images/felipe-avatar.jpg';
 import { Logo } from '../../components/Logo';
 import { LogoutButton } from '../../components/LogoutButton';
+import { AuthContext } from '../../contexts/AuthContext';
 import { NavBar } from '../NavBar';
 
 import * as S from './styles';
 
-export const Header = ({ isAuthenticated }) => (
-  <S.Container>
-    <div className="fixed-content">
-      <div className="logo-and-nav">
-        <Logo />
+export const Header = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <S.Container>
+      <div className="fixed-content">
+        <div className="logo-and-nav">
+          <Logo />
+          {isAuthenticated && (
+            <NavBar />
+          )}
+        </div>
+
         {isAuthenticated && (
-          <NavBar />
+          <div className="profile-and-logout">
+            <Link to="/profile" id="btn-avatar">
+              <img src={felipeAvatar} alt="Avatar" id="avatar" />
+            </Link>
+
+            <LogoutButton />
+          </div>
         )}
       </div>
-
-      {isAuthenticated && (
-        <div className="profile-and-logout">
-          <Link to="/profile" id="btn-avatar">
-            <img src={felipeAvatar} alt="Avatar" id="avatar" />
-          </Link>
-
-          <LogoutButton />
-        </div>
-      )}
-    </div>
-  </S.Container>
-);
-
-Header.propTypes = {
-  isAuthenticated: PropTypes.bool,
-};
-
-Header.defaultProps = {
-  isAuthenticated: true,
+    </S.Container>
+  );
 };
