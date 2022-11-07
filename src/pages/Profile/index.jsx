048aac7b36ff6d1e9d7aaa0f4fe11232bc504724
Felipstein <felipstein.oliveira@gmail.com>
2022-11-07 15:13:24 -0300

    feat: redirect to /profile/settings default

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import {
   User, Image, LockKey, Trash,
 } from 'phosphor-react';
@@ -10,43 +10,51 @@ import { NavSelecter } from '../../components/NavSelecter';
 
 import * as S from './styles';
 
-export const Profile = () => (
-  <S.Container>
-    <Heading>
-      Configurações —
-    </Heading>
-    <S.BottomContainer>
-      <S.AsideContainer>
-        <NavBar>
-          <NavSelecter
-            to="settings"
-            icon={User}
-          >
-            Meu perfil
-          </NavSelecter>
-          <NavSelecter
-            to="avatar"
-            icon={Image}
-          >
-            Meu avatar
-          </NavSelecter>
-          <NavSelecter
-            to="changepassword"
-            icon={LockKey}
-          >
-            Alterar senha
-          </NavSelecter>
-          <NavSelecter
-            to="deleteaccount"
-            icon={Trash}
-          >
-            Deletar conta
-          </NavSelecter>
-        </NavBar>
-      </S.AsideContainer>
-      <S.SectionContainer>
-        <Outlet />
-      </S.SectionContainer>
-    </S.BottomContainer>
-  </S.Container>
-);
+export const Profile = () => {
+  const location = useLocation();
+
+  if (location.pathname.split('/').filter((path) => path).length === 1) {
+    return <Navigate to="settings" />;
+  }
+
+  return (
+    <S.Container>
+      <Heading>
+        Configurações —
+      </Heading>
+      <S.BottomContainer>
+        <S.AsideContainer>
+          <NavBar>
+            <NavSelecter
+              to="settings"
+              icon={User}
+            >
+              Meu perfil
+            </NavSelecter>
+            <NavSelecter
+              to="avatar"
+              icon={Image}
+            >
+              Meu avatar
+            </NavSelecter>
+            <NavSelecter
+              to="changepassword"
+              icon={LockKey}
+            >
+              Alterar senha
+            </NavSelecter>
+            <NavSelecter
+              to="deleteaccount"
+              icon={Trash}
+            >
+              Deletar conta
+            </NavSelecter>
+          </NavBar>
+        </S.AsideContainer>
+        <S.SectionContainer>
+          <Outlet />
+        </S.SectionContainer>
+      </S.BottomContainer>
+    </S.Container>
+  );
+};
